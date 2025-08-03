161db86dfb13287a35e60997b7b7ada02f94e63c
brokenwifi9 <81181897+Mainakdey1@users.noreply.github.com>
2025-08-03 15:39:27 +0530

    some changes

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 #main fast api app
+from storage.document_store import Document_store
+from storage.inverted_index import InvertedIndex
+from search.tokenizer import tokenize
+
+doc_store = Document_store()
+index = InvertedIndex()
+
+
+def index_doc(doc_id : str, title : str, content : str):
+    full_text = f'{title} {content}'
+    tokens = tokenize(full_text)
+
+    doc_store.add_document(doc_id, {"id" : doc_id, "title" : title, "content" : content})
+    index.index_document(doc_id, tokens)
+
+def search(term : str):
+    results = index.search(term)
+    return [doc_store.get_document(doc_id) for score, doc_id in results]
+
+#---Test----
+if __name__ == "__main__":
+    index_doc("1", "Elasticsearch Basics", "Learn how it works")
+    index_doc("2", "Search Engines", "Elasticsearch is fast and scalable")
+    index_doc("3", "Full text search", "Search engines power modern apps")
+
+    print("\nSearch results for 'elasticsearch':")
+    for doc in search("elasticsearch"):
+        print(doc)
+
+    print("\nSearch results for 'search':")
+    for doc in search("search"):
+        print(doc)
--- storage/inverted_index.py
+++ storage/inverted_index.py
@@ -1,17 +1,39 @@
 #inverted logic application
 from collections import defaultdict
+import math
 
 class InvertedIndex:
     def __init__(self):
-        self.index = defaultdict(lambda: defaultdict(list))
+        self.index = defaultdict(lambda: defaultdict(int))
+        self.doc_lengths = defaultdict(int)
+        self.doc_freq = defaultdict(int)
+        self.num_docs = 0
 
-    def index_document(self, doc_id : str, text : str):
-        tokens = text.split()
-        for position, token in enumerate(tokens):
-            self.index[token][doc_id].append(position)
-    
-    def lookup(self, term : str):
-        return self.index.get(term, {})
+    def index_document(self, doc_id : str, tokens : list[str]):
+        self.num_docs += 1
+        seen_terms = set()
+        for token in tokens:
+            self.index[token][doc_id] += 1
+            self.doc_lengths[doc_id] += 1
+            if token not in self.doc_freq:
+                self.doc_freq[token] += 1
+                seen_terms.add(token)
+
+    def compute_tfidf(self, term : str, doc_id : str):
+        tf = self.index[term][doc_id] / self.doc_lengths[doc_id]
+        idf = math.log((self.num_docs + 1) / (1 + self.doc_freq[term])) + 1
+        return tf * idf
+
+    def search(self, term : str):
+        term = term.lower()
+        if term not in self.index:
+            return []
+        scores = []
+        for doc_id in self.index[term]:
+            score = self.compute_tfidf(term, doc_id)
+            scores.append((score, doc_id))
+        scores.sort(reverse= True)
+        return scores
 
     def all_terms(self):
         return self.index
