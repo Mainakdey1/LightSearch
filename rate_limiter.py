b262bab9516778973a9d9ff41862c75dc9c9888f
brokenwifi9 <81181897+Mainakdey1@users.noreply.github.com>
2025-12-18 17:44:13 +0530

    updated with jwt based user auth

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,18 +1,43 @@
 from fastapi import FastAPI, Request, HTTPException
 import redis
 import time
+from jose import jwt, JWTError
+from fastapi.security import HTTPBearer
  
 app = FastAPI()
+
+security = HTTPBearer()
+SECRET_KEY = 'secretasf'
+ALGORITHM = 'HS256'
+
 redis_client = redis.Redis(host = 'localhost', port=6379, db=0, decode_responses=True)
 
 RATE_LIMITS = {'/': 5, '/health' : 20}
 WINDOW_SIZE = 60
 
+def get_user_from_jwt(request: Request):
+    auth = request.headers.get('Authorization')
+    if not auth or not auth.startswith('Bearer'):
+        return None
+    token = auth.split(' ')[1]
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get('sub')
+    except JWTError:
+        return None
+    
+
 def rate_limiter(request: Request):
     client_ip = request.client.host
     path = request.url.path
+
+    user_id = get_user_from_jwt(request)
+
+    if user_id:
+        key = f'rate_limit:user:{user_id}:{path}'
+    else:
+        key = f'rate_limit:ip:{client_ip}:{path}'
     limit = RATE_LIMITS.get(path, 10)
-    key = f"rate_limit:{client_ip}:{path}"
     current = redis_client.get(key)
 
     if current is None:
